339154824577bb74da3895bd94c37f696a5c59b2
shivani-maurya5633 <shivanimaurya811282@gmail.com>
2025-09-04 21:29:19 +0530

    Update p31.php

--- p31.php
+++ p31.php
@@ -1,12 +1,1 @@
-<?php
-$countries = [
-    "India " => "New Delhi",
-    "USA" => "Washington, D.C. ",
-    "Japan" => "Tokyo"
-]; 
-echo "Original Array:\n";
-print_r($countries);
-$countries["Germany"] = "Berlin";
-echo "\nAfter adding Germany:\n";
-print_r($countries);
-?>
+
