ff1729ed2302c285fd3d9d4e6ca0979e4e52e991
shivani-maurya5633 <shivanimaurya811282@gmail.com>
2025-09-04 21:29:45 +0530

    Update p32.php

--- p32.php
+++ p32.php
@@ -1,12 +1,1 @@
-<?php
-$students = [
-      "Aman" => 85,
-      "Ravi" => 78,
-      "SIta"=> 92
-];
-echo "Original Marks:\n";
-print_r($students);
-$students["ravi"]= 88;
-echo "\nAfter Updating Ravi's Marks:\n";
-print_r($students);
-?>
+
